c133776b35b4418b275c18e8391f01107e9ba9bd
André Malcher <am4491@columbia.edu>
2015-12-15 22:47:16 +0100

    ifort crashing when compiling with simd directive

--- examples/tsunami/chile2010/rpn2_geoclaw_soa.f90
+++ examples/tsunami/chile2010/rpn2_geoclaw_soa.f90
@@ -50,16 +50,18 @@ subroutine rpn2(ixy,maxm,meqn,mwaves,maux,mbc,mx, &
 
     !input
     integer :: maxm,meqn,maux,mwaves,mbc,mx,ixy
-
+    
     !INPUT
     ! TODO: Question: Is meqn,mwaves,maux ever != 3? Inconsistent in code
     ! If not, some optimizations possible
-    !double precision :: ql(1-mbc:maxm+mbc, meqn)
-    !double precision :: qr(1-mbc:maxm+mbc, meqn)
-    double precision :: q(1-mbc:maxm+mbc, meqn)
-    double precision :: aux(1-mbc:maxm+mbc,maux)
-    !double precision :: auxl(1-mbc:maxm+mbc,maux)
-    !double precision :: auxr(1-mbc:maxm+mbc,maux)
+    double precision :: hL(1-mbc:maxm+mbc-1), hR(1-mbc:maxm+mbc-1)
+    double precision :: huL(1-mbc:maxm+mbc-1), huR(1-mbc:maxm+mbc-1)
+    double precision :: hvL(1-mbc:maxm+mbc-1), hvR(1-mbc:maxm+mbc-1)
+    double precision :: uL(1-mbc:maxm+mbc-1), uR(1-mbc:maxm+mbc-1)
+    double precision :: vL(1-mbc:maxm+mbc-1), vR(1-mbc:maxm+mbc-1)
+    double precision :: phiL(1-mbc:maxm+mbc-1), phiR(1-mbc:maxm+mbc-1)
+    double precision :: bL(1-mbc:maxm+mbc-1), bR(1-mbc:maxm+mbc-1)
+    double precision :: sE1(1-mbc:maxm+mbc-1), sE2(1-mbc:maxm+mbc-1)
 
     !OUTPUT
     ! TODO: Change order of s and fwave arrays -> Both are output
@@ -78,9 +80,8 @@ subroutine rpn2(ixy,maxm,meqn,mwaves,maux,mbc,mx, &
     ! TODO: Check whether operations on these variables can be vectorized at all
     ! if they're not declared as arrays!!! => Maybe work on original q? Shouldn't
     ! have any consequences as long as q is copied like here...
-    double precision :: hR,hL,huR,huL,uR,uL,hvR,hvL,vR,vL,phiR,phiL
     double precision :: sqrt_ghL, sqrt_ghR ! Temporary variables for sqrt(g*h)
-    double precision :: bR,bL,sL,sR,sRoe1,sRoe2,sE1,sE2,uhat,chat
+    double precision :: sL,sR,sRoe1,sRoe2,uhat,chat
     double precision :: s1m,s2m
     double precision :: hstar,hstartest,hstarHLL,sLtest,sRtest
     double precision :: tw,dxdc
@@ -97,40 +98,6 @@ subroutine rpn2(ixy,maxm,meqn,mwaves,maux,mbc,mx, &
     ! !$OMP SIMD -> This one seems to be extremely unefficient due
     ! to strided accesses and misalignment
 
-!!! AoS to SoA SECTION !!! TODO: This should be done in step2 already, when
-    !copying the 2D array to 1D!
-    !copy AoS arrays for ql/qr into SoA array
-
-    ! dir$ simd
-    do m = 1,meqn
-        do i = 1-mbc,maxm+mbc
-            q(i,m) = ql_aos(m,i)
-            !qr(i,m) = qr_aos(m,i)
-        enddo
-    enddo
-
-    do i = 1,maux
-        ! TODO how does the colon operator behave? Seems to hamper
-        ! autovectorization.. (spurious flow/anti dependence)
-        aux(:,i) = auxl_aos(i,:)
-        !auxr(:,i) = auxr_aos(i,:)
-    enddo
-!!! AoS to SoA SECTION !!!
-
-    !Initialize Riemann problem for grid interface
-    ! TODO: Better approach? Maybe uninitialized arrays have default value?
-
-    ! dir$ simd
-    do i=2-mbc,mx+mbc
-        do mw=1,mwaves ! TODO: F-wave dummy element for vectorization?
-            ! Only if mwaves always = 3
-            s(mw,i)=0.d0
-            fwave(1,mw,i)=0.d0
-            fwave(2,mw,i)=0.d0
-            fwave(3,mw,i)=0.d0
-        enddo
-    enddo
-    
     !set normal direction
     if (ixy.eq.1) then
         mu=2
@@ -139,153 +106,140 @@ subroutine rpn2(ixy,maxm,meqn,mwaves,maux,mbc,mx, &
         mu=3
         nv=2
     endif
-            
-    !zero (small) negative values if they exist
-#if 0
-    ! $omp simd
-    do m=1,meqn
-        ! Shifted by -1 => TODO: Merge loops for ql/qr and peel boundaries
-        do i=1-mbc,mx+mbc-1 
-            if (qr(i, m).lt.0.d0) then
-                qr(i, m)=0.d0
-                !qr(i, 2)=0.d0
-                !qr(i, 3)=0.d0
-            endif ! TODO: else qr(i,m) = qr(i,m) necessary for blending?
-        enddo
-    enddo
-    ! $omp end simd
-    ! $omp simd
-    do m=1,meqn
-        do i=2-mbc,mx+mbc
-            if (q(i, m).lt.0.d0) then
-                q(i, m)=0.d0
-            !    ql(i, 2)=0.d0
-            !    ql(i, 3)=0.d0
-            endif
-        enddo
-    enddo
-    ! $omp end simd
-#endif
-    do m=1,meqn
-        do i=1-mbc,mx+mbc
-            if (q(i, m).lt.0.d0) then
-                q(i, m)=0.d0
-            endif
+!!! AoS to SoA SECTION !!!    
+    do m = 1,meqn
+        do i = 1-mbc,maxm+mbc-1
+            hL(i) = ql_aos(1,i)
+            hR(i) = ql_aos(1, i+1) 
+            huL(i) = ql_aos(mu,i)
+            huR(i) = ql_aos(mu,i+1)
+            hvL(i) = ql_aos(nv,i)
+            hvR(i) = ql_aos(nv,i+1)
         enddo
     enddo
 
-    !loop through Riemann problems at each grid cell
-    do i=2-mbc,mx+mbc
-        !-----------------------Initializing-----------------------------------
-        !inform of a bad riemann problem from the start
-        if((q(i-1, 1).lt.0.d0).or.(q(i, 1) .lt. 0.d0)) then
-            write(*,*) 'Negative input: hl,hr,i=',q(i-1, 1),q(i, 1),i
+    bL(:) = auxl_aos(1,1-mbc:mx+mbc-1)
+    bR(:) = auxl_aos(1,2-mbc:mx+mbc)
+!!! AoS to SoA SECTION !!!
+    
+    fwave = 0.d0 ! TODO: Does this and the next line work as expected? (i.e. sets whole array to 0?)
+    s = 0.d0
+   
+    !TODO: inform of a bad riemann problem from the start
+    !zero (small) negative values if they exist => TODO: use built-in "where (q < 0.d0) q = 0.d0"?
+    do i=1-mbc,mx+mbc
+        if (hL(i) .lt. drytol) then
+            ! This case uses <drytol rather than <0.d0, eases next loop
+            hL(i) = 0.d0
+            huL(i) = 0.d0
+            hvL(i) = 0.d0
+            uL(i) = 0.d0
+            vL(i) = 0.d0
+            phiL(i) = 0.d0
+        else ! This could also be else part of if() in the previous loop
+            ! Need to check whether this would be efficient though (no blending operation!)
+            ! TODO: Next six lines really required for masking?
+            hL(i) = hL(i)
+            huL(i) = huL(i)
+            hvL(i) = hvL(i)
+            uL(i)   = huL(i) / hL(i)
+            vL(i)   = hvL(i) / hL(i)
+            phiL(i) = 0.5d0*g*hL(i)*hL(i) + huL(i)*huL(i)/hL(i)
         endif
+        if (hR(i) .lt. drytol) then
+            ! This case uses <drytol rather than <0.d0, eases next loop
+            hR(i) = 0.d0
+            huR(i) = 0.d0
+            hvR(i) = 0.d0
+            uR(i) = 0.d0
+            vR(i) = 0.d0
+            phiR(i) = 0.d0
+        else ! This could also be else part of if() in the previous loop
+            ! Need to check whether this would be efficient though (no blending operation!)
+            ! TODO: Next six lines really required for masking?
+            hR(i) = hR(i)
+            huR(i) = huR(i)
+            hvR(i) = hvR(i)
+            uR(i)   = huR(i) / hR(i)
+            vR(i)   = hvR(i) / hR(i)
+            phiR(i) = 0.5d0*g*hR(i)*hR(i) + huR(i)*huR(i)/hR(i)
+        endif
+    enddo
+
+    !loop through Riemann problems at each grid cell
+    !$omp simd private(wall, hstar,hstartest, s1m, s2m, rare1, rare2, &
+    !$omp&      sqrt_ghL, sqrt_ghR, sL, sR, chat, uhat, sRoe1, SRoe2, maxiter, &
+    !$omp&      drytol, g, sw, fw)
+    do i=1-mbc,mx+mbc-1 ! mx+2mbc-1 Riemann problems, where i corresponds to i-th Riemann problem
+        !-----------------------Initializing-----------------------------------
 
         !skip problem if in a completely dry area
         ! Check: cycle/continue problem for vectorization?
         ! => YES! See https://software.intel.com/en-us/node/524555
-        if (q(i-1, 1) <= drytol .and. q(i, 1) <= drytol) then
-            cycle
-        endif
+        !if (hL(i) <= drytol .and. hR(i) <= drytol) then
+        !    cycle
+        !endif
 
         !Riemann problem variables
-        hL = q(i-1, 1) 
-        hR = q(i, 1) 
-        huL = q(i-1, mu) 
-        huR = q(i, mu) 
-        bL = aux(i-1,1)
-        bR = aux(i,1)
+        wall(1:3) = 1.d0
+        
+        if (hR(i).le.drytol) then
+            !dir$ forceinline
+            call riemanntype(hL(i),hL(i),uL(i),-uL(i),hstar,s1m,s2m,rare1,rare2,1,drytol,g)
+            hstartest=max(hL(i),hstar)
 
-        hvL=q(i-1, nv) 
-        hvR=q(i, nv)
-
-        !check for wet/dry boundary
-        if (hR.gt.drytol) then
-            uR=huR/hR
-            vR=hvR/hR
-            phiR = 0.5d0*g*hR**2 + huR**2/hR
-        else
-            hR = 0.d0
-            huR = 0.d0
-            hvR = 0.d0
-            uR = 0.d0
-            vR = 0.d0
-            phiR = 0.d0
-        endif
-
-        if (hL.gt.drytol) then
-            uL=huL/hL
-            vL=hvL/hL
-            phiL = 0.5d0*g*hL**2 + huL**2/hL
-        else
-            hL=0.d0
-            huL=0.d0
-            hvL=0.d0
-            uL=0.d0
-            vL=0.d0
-            phiL = 0.d0
-        endif
-
-        ! TODO: wall(1:3) faster/slower?
-        wall(1) = 1.d0
-        wall(2) = 1.d0
-        wall(3) = 1.d0
-        if (hR.le.drytol) then
-            call riemanntype(hL,hL,uL,-uL,hstar,s1m,s2m,rare1,rare2,1,drytol,g)
-            hstartest=max(hL,hstar)
-
-            if (hstartest+bL.lt.bR) then !right state should become ghost values that mirror left for wall problem
+            if (hstartest+bL(i).lt.bR(i)) then !right state should become ghost values that mirror left for wall problem
                 wall(2)=0.d0
                 wall(3)=0.d0
-                hR=hL
-                huR=-huL
-                bR=bL
-                phiR=phiL
-                uR=-uL
-                vR=vL
-            elseif (hL+bL.lt.bR) then
-                bR=hL+bL
+                hR(i)=hL(i)
+                huR(i)=-huL(i)
+                bR(i)=bL(i)
+                phiR(i)=phiL(i)
+                uR(i)=-uL(i)
+                vR(i)=vL(i)
+            elseif (hL(i)+bL(i).lt.bR(i)) then
+                bR(i)=hL(i)+bL(i)
             endif
-        elseif (hL.le.drytol) then ! right surface is lower than left topo
-            call riemanntype(hR,hR,-uR,uR,hstar,s1m,s2m,rare1,rare2,1,drytol,g)
-            hstartest=max(hR,hstar)
+        elseif (hL(i).le.drytol) then ! right surface is lower than left topo
+            !dir$ forceinline
+            call riemanntype(hR(i),hR(i),-uR(i),uR(i),hstar,s1m,s2m,rare1,rare2,1,drytol,g)
+            hstartest=max(hR(i),hstar)
 
-            if (hstartest+bR.lt.bL) then  !left state should become ghost values that mirror right
+            if (hstartest+bR(i).lt.bL(i)) then  !left state should become ghost values that mirror right
                 wall(1)=0.d0
                 wall(2)=0.d0
-                hL=hR
-                huL=-huR
-                bL=bR
-                phiL=phiR
-                uL=-uR
-                vL=vR
-            elseif (hR+bR.lt.bL) then
-                bL=hR+bR
+                hL(i)=hR(i)
+                huL(i)=-huR(i)
+                bL(i)=bR(i)
+                phiL(i)=phiR(i)
+                uL(i)=-uR(i)
+                vL(i)=vR(i)
+            elseif (hR(i)+bR(i).lt.bL(i)) then
+                bL(i)=hR(i)+bR(i)
             endif
         endif
 
         !determine wave speeds
-        sqrt_ghL = sqrt(g*hL)
-        sqrt_ghR = sqrt(g*hR)
-        sL=uL-sqrt_ghL ! 1 wave speed of left state
-        sR=uR+sqrt_ghR ! 2 wave speed of right state
+        sqrt_ghL = sqrt(g*hL(i))
+        sqrt_ghR = sqrt(g*hR(i))
+        sL=uL(i)-sqrt_ghL ! 1 wave speed of left state
+        sR=uR(i)+sqrt_ghR ! 2 wave speed of right state
 
-        uhat=(sqrt_ghL*uL + sqrt_ghR*uR)/(sqrt_ghR+sqrt_ghL) ! Roe average
-        chat=sqrt(g*0.5d0*(hR+hL)) ! Roe average
+        uhat=(sqrt_ghL*uL(i) + sqrt_ghR*uR(i))/(sqrt_ghR+sqrt_ghL) ! Roe average
+        chat=sqrt(g*0.5d0*(hR(i)+hL(i))) ! Roe average
         sRoe1=uhat-chat ! Roe wave speed 1 wave
         sRoe2=uhat+chat ! Roe wave speed 2 wave
 
-        sE1 = min(sL,sRoe1) ! Eindfeldt speed 1 wave
-        sE2 = max(sR,sRoe2) ! Eindfeldt speed 2 wave
+        sE1(i) = min(sL,sRoe1) ! Eindfeldt speed 1 wave
+        sE2(i) = max(sR,sRoe2) ! Eindfeldt speed 2 wave
 
         !--------------------end initializing...finally----------
         !solve Riemann problem.
 
         maxiter = 1
 
-        call riemann_fwave(meqn,mwaves,hL,hR,huL,huR,hvL,hvR, &
-            bL,bR,uL,uR,vL,vR,phiL,phiR,sE1,sE2,drytol,g,sw,fw)
+        call riemann_fwave(meqn,mwaves,hL(i),hR(i),huL(i),huR(i),hvL(i),hvR(i), &
+            bL(i),bR(i),uL(i),uR(i),vL(i),vR(i),phiL(i),phiR(i),sE1(i),sE2(i),drytol,g,sw,fw)
 
         ! eliminate ghost fluxes for wall
         do mw=1,3
@@ -305,14 +259,15 @@ subroutine rpn2(ixy,maxm,meqn,mwaves,maux,mbc,mx, &
             fwave(nv,mw,i) = fw(3,mw)
         enddo
     enddo
+    !$omp end simd
 
     !==========Capacity for mapping from latitude longitude to physical space====
     if (mcapa.gt.0) then
-        do i=2-mbc,mx+mbc
+        do i=1-mbc,mx+mbc-1
             if (ixy.eq.1) then
                 dxdc=(earth_radius*deg2rad)
             else
-                dxdc=earth_radius*cos(aux(i,3))*deg2rad
+                dxdc=earth_radius*cos(auxr_aos(i,3))*deg2rad
             endif
 
             do mw=1,mwaves
